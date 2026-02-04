9d5166ae92e506379197ff11c12b0bb89e45f4da
aman rawat <rawataman2162001@gmail.com>
2026-02-04 16:29:34 +0530

    Fix math_operation imports and tests

--- src/math_operation.py
+++ src/math_operation.py
@@ -3,3 +3,6 @@ def add(a,b):
 #new output 
 def mul(a,b):
     return a*b
+#new file for subtraction
+def sub(a,b):
+    return a-b
--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,12 +1,14 @@
-from src.math_operation import add,mul
+from src.math_operation import add,mul,sub
 
 # writing the test cases for the add function
 
 def test_add():
     assert add(2,3) == 5  #check the add function whether the 2+3 == 5 , give true or false
-    assert add(-1,1) == 0
+    
 
 def test_mul():
     assert mul(2,3) == 6
-    assert mul(1,1) == 1
-    assert mul(2,3) == 6
+# new add this
+def test_sub():
+    assert sub(5,3) == 2
+    
